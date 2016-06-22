a40d5a19b4e62ce8b8845ad22bd57dbcae73c906
Matthew Wright <matthew@smallhelm.com>
2016-06-22 11:03:31 -0600

    staring test for n_surrounding_lines

--- tests.js
+++ tests.js
@@ -1,7 +1,8 @@
+var _ = require('lodash');
 var test = require('tape');
 var diff = require('./');
 
-test('diff-lines', function(t){
+test('basics', function(t){
   var a = '';
   a += 'one\n';
   a += 'two\n';
@@ -10,11 +11,41 @@ test('diff-lines', function(t){
   var b = '';
   b += 'one\n';
   b += 'three\n';
+  b += 'four\n';
 
   var expected = '';
   expected += ' one\n';
   expected += '-two\n';
-  expected += ' three';
+  expected += ' three\n';
+  expected += '+four';
+
+  t.equals(diff(a, b), expected);
+
+  t.end();
+});
+
+test('n_surrounding_lines', function(t){
+  var lines = _.map(_.range(0, 100), function(i){
+    return 'line ' + i;
+  });
+
+  var a = lines.join('\n');
+  var b = _.map(lines, function(line, i){
+    if(i === 50){
+      return 'change ' + i;
+    }
+    return line;
+  }).join('\n');
+
+  var expected = _.flattenDeep(_.map(lines, function(line, i){
+    if(i === 50){
+      return [
+        '-line ' + i,
+        '+change ' + i
+      ];
+    }
+    return ' ' + line;
+  })).join('\n');
 
   t.equals(diff(a, b), expected);
 
